974e18a18ecc43e7a424e0adba8e267a51ad9a15
phung-ha <phunghaworkspace@gmail.com>
2024-04-02 00:09:07 -0400

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Fira_Code } from "next/font/google";
+import "./globals.css";
+
+const inter = Fira_Code({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "youtube-grep",
+  description: "Chat with your saved Youtube videos with the help of LLM",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
